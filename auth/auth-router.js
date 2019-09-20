9582be7799370e9b13bc4c5b89d2e3388f44c285
Bishop <lake.d.bishop@gmail.com>
2019-09-20 11:04:21 -0500

    Register complete

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
 
+const Users = require('./auth-model')
+const bcrypt = require('bcryptjs')
+
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body
+  
+  if (!user.username || !user.password) {
+    res.status(401).json({Message: "Please provide both a username and a password"})
+  } else {
+    const hash = bcrypt.hashSync(user.password, 12)
+    user.password = hash
+
+    Users.add(user)
+      .then(user => {
+        res.status(201).json(req.body)
+      })
+      .catch(error => {
+        res.status(500).json({Error: 'there was an issue createing the user account in the database'})
+      })
+  }
 });
 
 router.post('/login', (req, res) => {
